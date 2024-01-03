532a23e2bae9414f6dae2378d3d8b4d7991aa2ad
salehkarimy <salehkarimi423@gmail.com>
2024-01-03 13:43:54 +0330

    28-private-and-public-access

--- ts-app/dist/oop.js
+++ ts-app/dist/oop.js
@@ -2,16 +2,29 @@
 /**
  * 25-creating-class
  * 27-this-keyword
+ * 28-private-and-public-access
  */
 class Team {
     constructor(nameTeam) {
+        this.members = [];
         this.name = nameTeam;
     }
     describe() {
         console.log(`Team: ${this.name}`);
     }
+    addMember(member) {
+        this.members.push(member);
+    }
+    memberInfo() {
+        console.log(`members: ${this.members.length}`);
+        console.log(this.members);
+    }
 }
 const devTeam = new Team("Developer");
-devTeam.describe();
+// devTeam.describe();
+devTeam.addMember("Mohammad");
+devTeam.addMember("Joe");
+// devTeam.members[5] = "ali" // -> Error: Property 'members' is private and only accessible within class 'Team'.
+devTeam.memberInfo();
 const uiTeam = { name: "UI", describe: devTeam.describe };
-uiTeam.describe();
+// uiTeam.describe();
--- ts-app/src/oop.ts
+++ ts-app/src/oop.ts
@@ -1,21 +1,38 @@
 /**
  * 25-creating-class
  * 27-this-keyword
+ * 28-private-and-public-access
  */
 
 class Team {
-  name: string;
+  public name: string;
+  private members: string[] = [];
+
   constructor(nameTeam: string) {
     this.name = nameTeam;
   }
 
-  describe(this: Team) {
+  describe() {
     console.log(`Team: ${this.name}`);
+  }
+
+  addMember(member: string) {
+    this.members.push(member);
+  }
+
+  memberInfo() {
+    console.log(`members: ${this.members.length}`);
+    console.log(this.members);
   }
 }
 
 const devTeam = new Team("Developer");
-devTeam.describe();
+// devTeam.describe();
+
+devTeam.addMember("Mohammad");
+devTeam.addMember("Joe");
+// devTeam.members[5] = "ali" // -> Error: Property 'members' is private and only accessible within class 'Team'.
+devTeam.memberInfo();
 
 const uiTeam = { name: "UI", describe: devTeam.describe };
-uiTeam.describe();
+// uiTeam.describe();
