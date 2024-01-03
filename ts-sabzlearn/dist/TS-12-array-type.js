4418169e6c2fcbfe21e832d1d030d56adae462dc
salehkarimy <salehkarimi423@gmail.com>
2024-01-03 22:51:31 +0330

    TS-12-array-type

---
+++ ts-sabzlearn/dist/TS-12-array-type.js
@@ -0,0 +1,9 @@
+"use strict";
+let users = ["Ali", "Amin", "Sal3h"];
+let random = [
+    12,
+    "sal3h",
+    false,
+    ["ali", "hamid"],
+];
+users.push("mmd");
---
+++ ts-sabzlearn/src/TS-12-array-type.ts
@@ -0,0 +1,10 @@
+let users: string[] = ["Ali", "Amin", "Sal3h"];
+let random: (string | number | boolean | string[])[] = [
+  12,
+  "sal3h",
+  false,
+  ["ali", "hamid"],
+];
+
+users.push("mmd");
+
