f4e3d187514bde3ee65fca13c2e1b19f75f7e857
salehkarimy <salehkarimi423@gmail.com>
2024-01-03 23:43:02 +0330

    TS-20-template-literal-type

---
+++ ts-sabzlearn/dist/TS-20-template-literal-type.js
@@ -0,0 +1,5 @@
+"use strict";
+function setCssProperty(width) {
+    return width;
+}
+console.log(setCssProperty("34rem"));
---
+++ ts-sabzlearn/src/TS-20-template-literal-type.ts
@@ -0,0 +1,5 @@
+function setCssProperty(width: `${number}px` | `${number}em` | `${number}rem`) {
+  return width;
+}
+
+console.log(setCssProperty("34rem"));
