2bf3d62ff79deb4d9e895c65bb54199c92b3a6bd
salehkarimy <salehkarimi423@gmail.com>
2024-01-03 23:35:51 +0330

    TS-14-tuple-type

---
+++ ts-sabzlearn/dist/TS-14-tuple-type.js
@@ -0,0 +1,6 @@
+"use strict";
+let roles = [
+    [1, "Admin"],
+    [2, "user"],
+];
+console.log(roles);
---
+++ ts-sabzlearn/src/TS-14-tuple-type.ts
@@ -0,0 +1,7 @@
+let roles: [number, string][] = [
+  [1, "Admin"],
+  [2, "user"],
+];
+
+
+console.log(roles);
