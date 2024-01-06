4b7d2062573351296c15752603e34134c2a65ed0
salehkarimy <salehkarimi423@gmail.com>
2024-01-06 16:20:07 +0330

    TS-50-setter

---
+++ ts-sabzlearn/dist/TS-50-setter.js
@@ -0,0 +1,35 @@
+"use strict";
+// Getter - Setter
+class Person {
+    name;
+    email;
+    age;
+    constructor(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+}
+class Student extends Person {
+    _lessons;
+    constructor(name, email, age, _lessons) {
+        super(name, email, age);
+        this._lessons = _lessons;
+        this._lessons = _lessons;
+    }
+    get lessons() {
+        return this._lessons;
+    }
+    set lessons(lesson) {
+        this._lessons.push(lesson);
+    }
+}
+const aliStudent = new Student("Ali", "ali@gmail.com", 23, ["Math"]);
+const mmdPerson = new Person("MMD", "mmd@gmail.com", 30);
+console.log(aliStudent);
+aliStudent.lessons = "TS";
+aliStudent.lessons = "Nodejs";
+console.log(aliStudent.lessons);
---
+++ ts-sabzlearn/src/TS-50-setter.ts
@@ -0,0 +1,42 @@
+// Getter - Setter
+
+class Person {
+  constructor(
+    public name: string,
+    readonly email: string,
+    protected age: number
+  ) {
+    this.name = name;
+    this.email = email;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  constructor(
+    name: string,
+    email: string,
+    age: number,
+    private _lessons: string[]
+  ) {
+    super(name, email, age);
+    this._lessons = _lessons;
+  }
+
+  get lessons(): string[] {
+    return this._lessons;
+  }
+
+  set lessons(lesson: string) {
+    this._lessons.push(lesson);
+  }
+}
+
+const aliStudent = new Student("Ali", "ali@gmail.com", 23, ["Math"]);
+const mmdPerson = new Person("MMD", "mmd@gmail.com", 30);
+
+console.log(aliStudent);
+aliStudent.lessons = "TS";
+aliStudent.lessons = "Nodejs";
+
+console.log(aliStudent.lessons);
