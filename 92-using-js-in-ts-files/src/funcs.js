8c1a178853695a698bb7bdf3ddd03446b70dbf0f
Saleh Karimi <sal3hkarimi@gmail.com>
2024-04-18 05:18:00 +0330

    TS-95: js-doc

--- 92-using-js-in-ts-files/src/funcs.js
+++ 92-using-js-in-ts-files/src/funcs.js
@@ -1,5 +1,14 @@
-// @ts-noCheck
-
+/**
+ * Power method :)
+ * @param {number} num 
+ * @returns {number}
+ */
 export const pow = (num) => num ** 2
 
+/**
+ * Sum method :)
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns {number}
+ */
 export const sum = (num1, num2) => num1 + num2
