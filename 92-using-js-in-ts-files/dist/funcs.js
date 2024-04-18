f010a51bb80e03eaee1b2a181a0064d9524683f4
Saleh Karimi <sal3hkarimi@gmail.com>
2024-04-18 05:04:04 +0330

    TS-93-checkJS-option: check js file by typescript

--- 92-using-js-in-ts-files/dist/funcs.js
+++ 92-using-js-in-ts-files/dist/funcs.js
@@ -1,2 +1,2 @@
-export const pow = num => num ** 2;
+export const pow = (num) => num ** 2;
 export const sum = (num1, num2) => num1 + num2;
--- 92-using-js-in-ts-files/src/funcs.js
+++ 92-using-js-in-ts-files/src/funcs.js
@@ -1,4 +1,3 @@
-export const pow = num => num ** 2
-
+export const pow = (num) => num ** 2
 
 export const sum = (num1, num2) => num1 + num2
